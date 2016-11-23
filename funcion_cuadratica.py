37c63b654190f93b1e9dccf493ca9f8d9710dc5b
USUARIO06-PC\Usuario01 <dvale14toledo@gmail.com>
2016-11-23 13:11:08 -0500

    Guardar resultados en un archivo txt

---
+++ funcion_cuadratica.py
@@ -0,0 +1,37 @@
+##ESCUELA POLITECNICA NACIONAL
+##ESCUELA DE FORMACION DE TECNOLOGOS
+##ALGORITMOS FUNDAMENTALES
+##AUTOR:DIANA BONILLA-VALERIA OCHOA
+##TITULO: PRACTICA DE MANEJO DE ARCHIVOS
+import math
+archi=open('funcion.txt','a')
+print("\t\t\t     Escuela Politécnica Nacional")
+print("\t\t\t  Escuela de Formación de Tecnólogos")
+print("\t\t\t\t  Programación Avanzada")
+print("\t\t\t     Diana Bonilla - Valeria Ochoa")
+print("Ingrese los datos: ")
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+c = int(input("Ingrese el valor de c: "))
+
+raiz = b * b - 4 *a*c
+##while a |b |c != "":
+if raiz < 0:
+    print("Ingrese otros valores, la raiz es imaginaria: ", raiz)
+    archi.write("Ingrese otros valores, la raiz es imaginaria")
+else:
+    x1=(-b+(math.sqrt(raiz)))/(2*a)
+    x2=(-b-(math.sqrt(raiz)))/(2*a)
+    print("x1= ",str(x1),"x2= ",str(x2))
+    archi.write("X1= "+str(x1)+'\n')
+    archi.write("X2= "+str(x2)+'\n')
+   
+##print("\nIngrese los datos: ")
+##    a = int(input("Ingrese el valor de a: "))
+##    b = int(input("Ingrese el valor de b: "))
+##    c = int(input("Ingrese el valor de c: "))
+    
+##archi.write('hoola')
+
+archi.close()
+
